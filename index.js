dd06f0b7034793b9aeeeda7e5361ca5afb206c0e
Kevin <klums24@gmail.com>
2023-04-12 16:48:08 -0700

    event listener phase 1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("The DOM has loaded");
+});
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
+
+document.addEventListener("DOMContentLoaded", function(){
+    const text = document.querySelector("#text");
+    text.innerHTML = "This is really cool!";
+});
+
+
